0fe1a70206dc8ed7e094b8a0277fe922c7b2e396
Rafif Hikmatiar <rafifhkmtr@gmail.com>
2022-10-30 22:42:52 +0700

    feat: update source code

--- P1_Komnum_D16.py
+++ P1_Komnum_D16.py
@@ -5,48 +5,51 @@
 import matplotlib.pyplot as plot
 import numpy
 
-def f(x):
-    return x**3 - 3 * x + 1
 
-def bisection(x0, x1, e):
-    count = 1
+x0 = float(input('input x0: '))
+x1 = float(input('input x1: '))
+err = float(input('input error (semakin kecil semakin akurat): '))
+
+def f(x):
+    return x**3 - x**2 + 2*x + 1
+
+def bisection(x0, x1, err):
+    totIterasi = 1
     condition = True
 
     while condition:
-        x2 = (x0 + x1) / 2
-        print("Iteration-%d, x2 = %0.6f and f(x2) = %0.6f" %(count, x2, f(x2)))
+        xt = (x0 + x1) / 2
+        print("Iterasi-%d, xt = %0.6f dan f(xt) = %0.6f" %(totIterasi, xt, f(xt)))
 
-        if f(x0) * f(x2) < 0:
-            x1 = x2
+        if f(x1)*f(xt) > 0 :
+            x1 = xt
         else:
-            x0 = x2
+            x0 = xt
 
-        count += 1
-        condition = abs(f(x2)) > e
+        totIterasi += 1
+        condition = abs(f(xt)) > err
 
-    return x2
+    return xt
 
-x0 = int(input())
-x1 = int(input())
-e = float(input())
 
 if f(x0) * f(x1) > 0.0:
-    print("Given values do not bracket the root.")
-    print("Try Again with different guess values.")
+    print("Pengoperasian f(x) keluar dari constraint")
 else:
-    root = bisection(x0, x1, e)
-    print("\nRoot : %0.8f" %root)
+    akar = bisection(x0, x1, err)
+    print("\nHasil akar : %0.8f" %akar)
 
-x = numpy.arange(x0, x1, e)
+x = numpy.arange(x0, x1, err)
 y = f(x)
 
 def buildGraph():
-    plot.plot(x, y, color= "black", label= "graph", zorder= 0)
-    plot.scatter(root, f(root), color= "black", linewidths= 0.5, label="root", zorder= 1)
+    plot.plot(x, y, color = "#C94023", label="grafik fungsi", zorder=0)
+    plot.axvline(x=akar, ymin=0, ymax=1, color="cyan", zorder=1)
+    plot.scatter(akar, f(akar), color="blue", linewidths=0.5, label="akar", zorder=2)
     plot.xlabel("x-axis")
     plot.ylabel("y-axis")
-    plot.title("bolzano method")
+    plot.title("Grafik Fungsi Metode Bolzano")
     plot.legend()
+    plot.grid()
     plot.show()
 
 buildGraph()
